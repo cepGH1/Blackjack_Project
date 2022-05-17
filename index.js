7fb078d9b09f2d667745be2cfd47b6561883a388
clare <clarepritchard@hotmail.com>
2022-05-17 15:20:00 +0100

    afternoons work

--- index.js
+++ index.js
@@ -1,23 +1,71 @@
-
+'use strict'
 const saveElement = document.getElementById("draw-button");
 let header = document.querySelector("h1");
+const totalHeld = document.querySelector("#total-held");
+
+const drawButton = document.querySelector("#save-button");
+const testDiv = document.querySelector("#test");
+const startButton = document.querySelector("#start-button");
+
+startButton.addEventListener("click", start);
+drawButton.addEventListener("click", render);
+let handValues =[];
+let total = 0;
 
 function render() {
+   //testDiv.innerHTML = "hello world";
    let value = Math.floor((Math.random() * 13) + 1);
-   console.log(value);
-   header.innerText = value;
+
+   let output = "number is " + value;
+   alert(output);
+
+   
+   handValues.push(value);
+   total = total + value;
+   if(total >21){
+       totalHeld.innerHTML = "Bust";
+   }
+   else{
+   totalHeld.innerHTML = total;
+   }
+   
+        let newRow = document.createElement("tr");
+        let newCell = document.createElement("td");
+        let newEntry = document.createTextNode(value);
+        newCell.appendChild(newEntry);
+        newRow.appendChild(newCell);
+        document.getElementById("table1").appendChild(newRow)
+    
+
+   //console.log(value);
+   //header.innerText = value;
    
    
 
-    var newRow = document.createElement("tr");
-    var newCell = document.createElement("td");
-    newCell.innerHTML = value;
-    newRow.append(newCell);
-    document.getElementById("rows").appendChild(newRow);
+    //var newRow = document.createElement("tr");
+    //var newCell = document.createElement("td");
+   // newCell.innerHTML = value;
+    //newRow.append(newCell);
+    //document.getElementById("rows").appendChild(newRow);
 }
 
+function totalInHand(){
+    for (let i=0; i<handValues.length; i++){
+        let total = total + handValues[i];
+    }
+    totalHeld.value = total;
+}
 
-console.log(inputElement);
+function restart(){
+
+}
+
+function start(){
+    let but1 = document.createElement("button");
+    let buttonMessage1 = document.createTextNode("draw");
+    but1.appendChild(buttonMessage1);
+    divTest.innerHTML = "hello world";
+}
 
 /*blackjack logic
 when you click the draw button it displays the card/number you drawed
