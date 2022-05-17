622d83af545af03c5dbb2205b819dfa951ca4f0a
clare <clarepritchard@hotmail.com>
2022-05-17 23:33:54 +0100

    better layout

--- index.js
+++ index.js
@@ -2,55 +2,117 @@
 const saveElement = document.getElementById("draw-button");
 let header = document.querySelector("h1");
 const totalHeld = document.querySelector("#total-held");
-
+const dealerHeld = document.querySelector("#dealer-total");
+const resultDiv = document.querySelector("#result-div");
 const drawButton = document.querySelector("#save-button");
 const testDiv = document.querySelector("#test");
 const startButton = document.querySelector("#start-button");
 const restartButton = document.querySelector("#restart-button");
+const standButton = document.querySelector("#stand-button");
 
 
 drawButton.addEventListener("click", render);
-//startButton.addEventListener("click", start);
-//but1.addEventListener("click", render);
+startButton.addEventListener("click", deal);
+standButton.addEventListener("click", stand);
 restartButton.addEventListener("click", refresh);
 
 let handValues =[];
 let total = 0;
+let dealerTotal = 0;
 
-function render() {
-   //testDiv.innerHTML = "hello world";
-   let value = Math.floor((Math.random() * 13) + 1);
+function selectACard(){
+    let value = Math.floor((Math.random() * 13) + 1);
+    return value;
+}
 
-   let output = "number is " + value;
-
-   
-   handValues.push(value);
-   total = total + value;
-   if(total >21){
-       totalHeld.innerHTML = "You've gone Bust";
-   }
-   else{
-   totalHeld.innerHTML = "Your hand total is : " + total;
-   }
-   
-        let newRow = document.createElement("tr");
+function playerDeal(){
+    let punter1 = selectACard();
+    total = total + punter1;
+    let newRow = document.createElement("tr");
         let newCell = document.createElement("td");
-        let newEntry = document.createTextNode(value);
+        let newEntry = document.createTextNode(punter1);
         newCell.appendChild(newEntry);
         newRow.appendChild(newCell);
         document.getElementById("table1").appendChild(newRow)
-    
 
-   //console.log(value);
-   //header.innerText = value;
-   
-   
+}
 
-    //var newRow = document.createElement("tr");
-    //var newCell = document.createElement("td");
-   // newCell.innerHTML = value;
-    //newRow.append(newCell);
-    //document.getElementById("rows").appendChild(newRow);
+function dealerDeal(){
+    let dealer1 = selectACard();
+    dealerTotal = dealerTotal + dealer1;
+    let newRow = document.createElement("tr");
+        let newCell = document.createElement("td");
+        let newEntry = document.createTextNode(dealer1);
+        newCell.appendChild(newEntry);
+        newRow.appendChild(newCell);
+        document.getElementById("table2").appendChild(newRow)
+}
+
+function deal(){
+    playerDeal();
+    playerDeal();
+    displayPlayerTotal();
+    dealerDeal();
+    dealerDeal();
+    displayDealerTotal();
+}
+
+function displayPlayerTotal(){
+    if(total >21){
+        totalHeld.innerHTML = "You've gone Bust";
+        resultDiv.innerHTML = "Dealer Wins";
+
+    }
+    else{
+    totalHeld.innerHTML = "Your hand total is : " + total;
+    }
+}
+
+function displayDealerTotal(){
+    if(total >21){
+        totalHeld.innerHTML = " Bust ";
+    }
+    else{
+    dealerHeld.innerHTML = "Dealer total is : " + dealerTotal;
+    }
+        
+}
+
+function render() {
+    playerDeal();
+    displayPlayerTotal();
+    if(total>21){
+        resultDiv.innerHTML = "Dealer Wins";
+    }
+   
+      
+}
+
+function stand(){
+    if (dealerTotal<18){
+        dealerDeal();
+    }
+    displayDealerTotal();
+    giveFeedback();
+}
+
+function giveFeedback(){
+    if (total>21){
+        resultDiv.innerHTML = "Dealer wins";
+        
+    }
+    else if(dealerTotal>21){
+        resultDiv.innerHTML = "You win";
+    }
+    else if(dealerTotal > total){
+        resultDiv.innerHTML = "Dealer wins";
+    }
+    else if(dealerTotal == total){
+        resultDiv.innerHTML = "You win";
+    }
+    else{
+        resultDiv.innerHTML = "You win";
+    }
 }
 
 function totalInHand(){
